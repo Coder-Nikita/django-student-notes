8f63810b545c19d2cd9c2c4f96a1c4fb20b58043
Coder-Nikita <nikii0410@gmail.com>
2026-03-14 23:59:58 +0530

    fixed few templates

--- student_notes/settings.py
+++ student_notes/settings.py
@@ -102,9 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/Nikita18/student_notes/staticfiles'
-STATICFILES_DIR = []
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/"staticfiles"
+STATICFILES_DIR = [BASE_DIR/"static"]
 
 
 
